eac0e47c72a7b6662882fbf2d5ce8eea2846f61c
Phillip Olagoke <103788263+Adonisenny@users.noreply.github.com>
2023-08-29 10:42:33 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,9 @@ app.use(cors())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use('/uploads',express.static('uploads'))
+app.use(cors({
+    origin:["http://localhost/3000", "https://testingrumors.onrender.com"],
+}))
 
 
 //middlewares
