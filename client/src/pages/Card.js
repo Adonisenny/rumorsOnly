0dd0b760210059dcda969fca3a68b5115d74ed40
Phillip Olagoke <103788263+Adonisenny@users.noreply.github.com>
2024-10-12 09:18:50 +0100

    profile links

--- client/src/pages/Card.js
+++ client/src/pages/Card.js
@@ -144,7 +144,7 @@ useEffect(() =>{
         <div className="workout-details   bg-transparent" >
         <div >
         <p className='' style={{textWrap:'wrap',color:'white'}}>{rumor?.story}</p>
-        <i className='absolute right-[12px] bottom-[1px] bg-slate-800 rounded-[12px] p-[4px] text-white'>{rumor.postedBy}</i>
+       <Link> <i className='absolute right-[12px] bottom-[1px] bg-slate-800 rounded-[12px] p-[4px] text-white'>{rumor.postedBy}</i></Link>
         
        
         </div> 
